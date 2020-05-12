fe46383f3a89b6689b305a7e8833261aeda5df59
Bruno Foggia <bruno@vixs.com.br>
2020-05-12 13:03:12 -0300

    build

--- src/backbone-api-99xp.js
+++ src/backbone-api-99xp.js
@@ -1,8 +1,6 @@
 // Micro Service integrator plus advanced validation, formatting and control over proccess possibilities
 
 
-
-
 // CODE DOCUMENTED BELOW
 // --------------
 
@@ -24,140 +22,163 @@ var extended = {
     tokenField: 'accessToken',
     // list of methods available in the api and their configuration (paths, validations)
     methods: {
-//        auth: {
-//            public: true,
-//            path: '/a/b',
-//            validations: {},
-//            data: {
-//                user: '',
-//                pass: '',
-//            },
-////            method: {}, if I dont set it, it will use a generic one
-//            
-//        },
-//        _sample: Sample
-//        _sample: {
-//            path: '/a/b',
-//            validations: {}
-//        }
+        //        auth: {
+        //            public: true,
+        //            path: '/a/b',
+        //            validations: {},
+        //            data: {
+        //                user: '',
+        //                pass: '',
+        //            },
+        ////            method: {}, if I dont set it, it will use a generic one
+        //            
+        //        },
+        //        _sample: Sample
+        //        _sample: {
+        //            path: '/a/b',
+        //            validations: {}
+        //        }
     },
-//  default method applying JWT Auth
-//  might be necessary to overwrite it
+    //  default method applying JWT Auth
+    //  might be necessary to overwrite it
     setAuthHeader(o) {
-        if(!this.tokenField) return BackboneApi.dataError(`tokenField is not set`);
-        o.headers['Authorization'] = 'Bearer '+this.data[this.auth][this.tokenField];
-        
+        if (!this.tokenField) {
+            return BackboneApi.dataError('tokenField is not set');
+        }
+        o.headers.Authorization = 'Bearer ' + this.data[this.auth][this.tokenField];
+
         return o;
     },
-    initialize(p, o={}) {
+    initialize(p, o = {}) {
         this.setRouterParameters(o.req, o.res);
         this.options = _.defaults(_.omit(o, 'req', 'res'), {
             autoexec: true
         });
         this.data = {};
-        
+
         typeof this.options.method === 'string' && (this.options.method = [this.options.method]);
         this.options.autoexec && this.execAll();
     },
     execAll() {
         !this._methodsExecution && (this._methodsExecution = _.clone(this.options.method));
-        if(_.size(this._methodsExecution) === 0) {
+        if (_.size(this._methodsExecution) === 0) {
             this._methodsExecution = null;
             _.bind(this.options.success, this)();
             return;
         }
-//        
+        //        
         var method = this._methodsExecution.shift();
-        
-        this.exec(method, ()=>this.execAll(), this.options.error);
+
+        this.exec(method, () => this.execAll(), this.options.error);
     },
     exec(method, success, error) {
-//        !method && (method = this.options.method);
-        
+        //        !method && (method = this.options.method);
+
         var methodData = this.methodData(method);
-        
-        var vErr = this.validate(this.getMethodInput(methodData), {methodData: methodData});
-        if(vErr !== null) return this.validationErrors(vErr);
-        
+
+        var vErr = this.validate(this.getMethodInput(methodData), {
+            methodData: methodData
+        });
+        if (vErr !== null) {
+            return this.validationErrors(vErr);
+        }
+
         var fn = _.bind(this.callApi, this),
             calledFn = _.partial(fn, method, methodData, success, error);
         // if there's no authorization needed fn is called
-        if(methodData.public) return calledFn();
-        else if(!this.auth) return BackboneApi.dataError(`method "${method}" is not set as public but auth is not set`);
-        
+        if (methodData.public) {
+            return calledFn();
+        } else if (!this.auth) {
+            return BackboneApi.dataError(`method "${method}" is not set as public but auth is not set`);
+        }
+
         return this.exec(this.auth, calledFn);
     },
     // used to call api and trigger callbacks accordingly
     callApi(method, methodData, success, error) {
         this.setApiCall(methodData);
-        
+
         var o = {
             method: methodData.method,
             data: this.getMethodInput(methodData),
             headers: _.result2(methodData, 'headers', {}, [methodData], this)
         };
-        if(!methodData.public) {
+        if (!methodData.public) {
             o = this.setAuthHeader(o);
         }
 
-        var before = _.bind(typeof methodData.before === 'function' ? methodData.before : (c, o) => { c(o); }, this),
-            save = _.bind(_.partial((method, methodData, success, error, o) => {
-                this.listenToOnce(this, 'sync', _.bind(()=>{
-                    this.data[method] = this.attributes;
+        var before = _.bind(typeof methodData.before === 'function' ? methodData.before : (c, _o) => {
+                c(_o);
+            }, this),
+            save = _.bind(_.partial((_method, _methodData, _success, _error, _o) => {
+                this.listenToOnce(this, 'sync', _.bind(() => {
+                    this.data[_method] = this.attributes;
                     this.attributes = {};
 
                     try {
-                        typeof methodData.success === 'function' && _.bind(methodData.success, this)(o, this._req.body, methodData);
-                        typeof success === 'function' && _.bind(success, this)(o, this._req.body, methodData);
-                    } catch(e) {
-                        console.error('Internal Failure');
-                        console.log(e); 
-                        this._res.status(500).send({message: 'Internal Failure'});
+                        typeof _methodData.success === 'function' && _.bind(_methodData.success, this)(_o, this._req.body, _methodData);
+                        typeof _success === 'function' && _.bind(_success, this)(_o, this._req.body, _methodData);
+                    } catch (e) {
+                        // console.error('Internal Failure');
+                        // console.error(e);
+                        this._res.status(500).send({
+                            message: 'Internal Failure'
+                        });
                     }
                 }, this));
-                this.listenToOnce(this, 'error', _.bind(()=>{
-                    !this._reqErr.response && console.error('Internal Failure');
+                this.listenToOnce(this, 'error', _.bind(() => {
+                    // !this._reqErr.response && console.error('Internal Failure');
                     try {
-                        typeof methodData.error === 'function' && _.bind(methodData.error, this)(this._reqErr.response, o, this._req.body, methodData);
-                        typeof error === 'function' ? _.bind(error, this)(this._reqErr.response || null, o, this._req.body, methodData) : !this._res._headerSent && this._res.status(this._reqErr.response ? this._reqErr.response.status : 500).send(this._reqErr.response ? this._reqErr.response.data : null);
-                    } catch(e) {
-                        console.error('Internal Failure');
-                        console.log(e); 
-                        this._res.status(500).send({message: 'Internal Failure'});
+                        typeof _methodData.error === 'function' && _.bind(_methodData.error, this)(this._reqErr.response, _o, this._req.body, _methodData);
+                        typeof _error === 'function' ? _.bind(_error, this)(this._reqErr.response || null, _o, this._req.body, _methodData) : 
+                            !this._res._headerSent && this._res.status(this._reqErr.response ? this._reqErr.response.status : 500).send(this._reqErr.response ? this._reqErr.response.data : null);
+                    } catch (e) {
+                        // console.error('Internal Failure');
+                        // console.error(e);
+                        this._res.status(500).send({
+                            message: 'Internal Failure'
+                        });
                     }
                 }, this));
 
-                this.save(null, o);
+                this.save(null, _o);
             }, method, methodData, success, error), this);
 
         try {
             before(save, o);
-        } catch(e) {
-            console.error('Internal Failure');
-            console.log(e); 
-            this._res.status(500).send({message: 'Internal Failure'});
+        } catch (e) {
+            // console.error('Internal Failure');
+            // console.log(e);
+            this._res.status(500).send({
+                message: 'Internal Failure'
+            });
         }
     },
     test: false,
     // getter for methods data. ensure data for method asked is set
     methodData(method) {
-        if(!method || !_.result(this, 'methods')[method])
-            return BackboneApi.dataError("Make sure you've set your method and it's data");
-        
+        if (!method || !_.result(this, 'methods')[method]) {
+            return BackboneApi.dataError('Make sure you\'ve set your method and it\'s data');
+        }
+
         var methodData = _.result(this, 'methods')[method] || {};
-        
-        if(!methodData.path) BackboneApi.dataError("Make sure you've set a path for method '"+method+"'");
-        
+
+        if (!methodData.path) {
+            BackboneApi.dataError('Make sure you\'ve set a path for method ' + method + '\'');
+        }
+
         methodData = this.setHttpMethod(methodData);
-        
+
         return methodData;
     },
     // get HTTP method from method configuration. if needed set a default HTTP method accordingly to method data input
     setHttpMethod(methodData) {
         var data = this.getMethodInput(methodData);
-        methodData = _.defaults(methodData, {method: (typeof data === 'object' && _.size(data)>0 ? 'POST' : 'GET') });
+        methodData = _.defaults(methodData, {
+            method: (typeof data === 'object' && _.size(data) > 0 ? 'POST' : 'GET')
+        });
         methodData.method = methodData.method.toUpperCase();
-        
+
         return methodData;
     },
     // get "data" from method configuration (can be an object or function) or req.body instead
@@ -167,8 +188,8 @@ var extended = {
     // replaces common behavior of backbone to ensure no id nonwanted will be added in Api URLs
     url() {
         return _.result(this, 'urlRoot') ||
-               _.result(this.collection, 'url') ||
-               BackboneApi.urlError();
+            _.result(this.collection, 'url') ||
+            BackboneApi.urlError();
     },
     // set asked method data for the request to be executed
     setApiCall(methodData) {
@@ -177,19 +198,25 @@ var extended = {
     // merge urlHost with the correct path for method asked. also render path as a template to make possible having attributes on it
     setApiUrl(path, methodData) {
         var host = _.result(this, 'urlHost') || BackboneApi.urlError(1),
-            data = _.extend({}, _.clone(this.data), {_model: this, _input: this.getMethodInput(methodData), _params: this._req.params});
-        
+            data = _.extend({}, _.clone(this.data), {
+                _model: this,
+                _input: this.getMethodInput(methodData),
+                _params: this._req.params
+            }),
+            pathfix = new RegExp('((?<!\:)\/{1,})', 'g');
+
         path = _.template(path)(data);
-        return this.urlRoot = [host, path].join('/').replace(/((?<!\:)\/{1,})/g, '/');
+
+        return this.urlRoot = [host, path].join('/').replace(pathfix, '/');
     },
-    sync: function(method, model, options) {
-      method = methodMap[options.method];
-      var args = [method, model, options];
-      return BackboneRequest.sync.apply(this, args);
+    sync(method, model, options) {
+        method = methodMap[options.method];
+        var args = [method, model, options];
+        return BackboneRequest.sync.apply(this, args);
     },
     validations(a, o) {
-        var v = _.result2(o.methodData, 'validations', {}, [a, o], this);
-        return v;
+        var vl = _.result2(o.methodData, 'validations', {}, [a, o], this);
+        return vl;
     },
     _validate() {
         return true;
@@ -201,29 +228,29 @@ var extended = {
         });
     },
     setRouterParameters(req, res) {
-        if (!req || !res) BackboneApi.dataError('Initialize requires an object with req and res (express route variables) within');
+        if (!req || !res) {
+            BackboneApi.dataError('Initialize requires an object with req and res (express route variables) within');
+        }
 
         this._req = req;
         this._res = res;
     },
 };
 
-_.map(['Model','Collection'], (x) => {
+_.map(['Model', 'Collection'], (x) => {
     BackboneApi[x] = BackboneRequest[x].extend(_.extend(_.clone(v), extended));
 });
 
-
-
 // Throw an error when a URL is needed, and none is supplied.
-BackboneApi.urlError = function (code) {
-    if(code==2) {
+BackboneApi.urlError = function(code) {
+    if (code === 2) {
         BackboneApi.dataError('A "path" property must be specified in methods list for the current method');
     }
     BackboneApi.dataError('A "urlHost" property or function must be specified');
 };
 
 // Throw an error when some DATA is needed, and none is supplied.
-BackboneApi.dataError = function (msg) {
+BackboneApi.dataError = function(msg) {
     throw new Error(msg);
 };
 
